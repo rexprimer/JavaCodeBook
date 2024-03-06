09b0e329c553772f9c505b99c4279fa0dde85765
Vikash Chaurasia <38579650+rexprimer@users.noreply.github.com>
2024-03-06 07:51:56 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,13 @@
+/*
+ * Create an node for LinkedList
+ * Node must have and VALUE and next pointer;
+ */
+public class Node{
+    int value;
+    Node next;
+    Node (int value)
+    {
+        this.value = value;
+    }
+
+}
